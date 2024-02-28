2b800acb6c3e0b6a5c04b19ca3d4476d3bea3bcd
Fareed-Shah <syedfareed667@gmail.com>
2024-02-28 22:36:01 +0500

    detail page from detail api

--- lib/UI/book_detail.dart
+++ lib/UI/book_detail.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_books_api/models/book.dart';
+import 'package:http/http.dart' as http;
 
 class BookDetail extends StatefulWidget {
   final Book book;
@@ -11,16 +14,49 @@ class BookDetail extends StatefulWidget {
 }
 
 class _BookDetailState extends State<BookDetail> {
+  late Book _book;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _book = widget.book;
+    _getbookDetail();
+  }
+
+  _getbookDetail() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      var url =
+          Uri.parse("https://www.googleapis.com/books/v1/volumes/${_book.id}");
+      var responce = await http.get(url);
+      var json = jsonDecode(responce.body);
+      setState(() {
+        _book = Book.fromjson(json);
+      });
+    } catch (e) {
+      setState(() {
+        _book = widget.book;
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(widget.book.volumeInfo?.title ?? ""),
+        title: Text(_book.volumeInfo?.title ?? ""),
       ),
       body: Center(
-        child:
-            Image.network(widget.book.volumeInfo?.imageLinks?.thumbnail ?? ""),
+        child: Image.network(_book.volumeInfo?.imageLinks?.thumbnail ?? ""),
       ),
     );
   }
